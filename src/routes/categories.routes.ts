73faa426637da8fa201eb50ec53f4b5e5c99f0ab
joao.barreto <joao.barreto@itbam.org.br>
2021-08-12 18:19:06 -0400

    inserindo o unique id

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { v4 as uuidV4 } from 'uuid';
+
+const categoriesRoutes = Router()
+
+const categories = [];
+
+categoriesRoutes.post("/", (request, response) => {
+    const { name, description } = request.body;
+    
+    const categorie = {
+        id: uuidV4(),
+        name,
+        description
+    }
+
+    categories.push(categorie);
+
+    return response.status(201).send()
+})
+
+export { categoriesRoutes };
