6a734f2a2c2c06b26aaeabf965966e6eec0f55ad
joaomenna1 <joaomenna1@mail.com>
2021-09-04 02:49:48 -0400

    concepts repository and dto

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,28 @@
-import { Router } from "express";
-import { v4 as uuidV4 } from 'uuid';
+import { request, Router } from "express";
+import { CategoriesRepository } from "../repositories/CategoriesRepository.";
+
 
 const categoriesRoutes = Router()
-
-const categories = [];
+const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
+
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+        return response.status(400).json({ error: "Already exist this category" })
+    }
+
+    categoriesRepository.create({name, description})
     
-    // const categorie = {
-    //     id: uuidV4(),
-    //     name,
-    //     description
-    // }
-
-    // categories.push(categorie);
-
     return response.status(201).send()
+})
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list()
+
+    return response.status(201).json(all)
 })
 
 export { categoriesRoutes };
