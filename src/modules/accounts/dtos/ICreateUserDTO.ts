ee0f238ac250a38b32364b83b3ba4ab6146cbb6e
joao.barreto <joao.barreto@itbam.org.br>
2021-09-28 16:25:19 -0400

    create repository user

---
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -0,0 +1,9 @@
+interface ICreateUserDTO {
+  name: string;
+  username: string;
+  password: string;
+  email: string;
+  driver_licenses: string;
+}
+
+export { ICreateUserDTO };
---
+++ src/modules/accounts/useCase/createUser/CreateUserController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+class CreateUserController {
+  handle(request: Request, reponse: Response): Promise<Response> {
+    // return response.status(200).send();
+  }
+}
+
+export { CreateUserController };
