3ae2714341ef1c90c7dff0c7711dcb202e2fc62e
joao.barreto <joao.barreto@itbam.org.br>
2021-10-04 10:52:44 -0400

    criptografar senha

--- src/modules/accounts/useCase/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCase/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -16,9 +17,17 @@ class CreateUserUseCase {
     email,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exist");
+    }
+
+    const password_hash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
-      password,
+      password: password_hash,
       email,
       driver_license,
     });
