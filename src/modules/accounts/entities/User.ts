3329ade5a0b274c76d82e1a43acff20f5945bf87
joao.barreto <joao.barreto@itbam.org.br>
2021-10-04 10:17:54 -0400

    fix inject dependency model user

--- src/modules/accounts/entities/User.ts
+++ src/modules/accounts/entities/User.ts
@@ -1,7 +1,7 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
-Entity("users");
+@Entity("users")
 class User {
   @PrimaryColumn()
   id: string;
